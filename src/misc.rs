381686040867b2b957e6c1aa04fdd9841b6f5614
TheEyeOfAr3s <eherzig@protonmail.com>
2023-01-09 09:58:42 +0100

    get_byte_from_input now returns 0 upon EOF/EOT

--- src/misc.rs
+++ src/misc.rs
@@ -4,6 +4,12 @@ use std::io::Read;
 pub fn get_byte_from_input() -> u8 { // Get a single byte from stdin
     match std::io::stdin().bytes().next().and_then(|result| result.ok()) {
         None => return 0,
-        Some(v) => return v
+        Some(v) => {
+            if v == 4 {
+                return 0;
+            } else {
+                return v;
+            };
+        }
     }
 }
